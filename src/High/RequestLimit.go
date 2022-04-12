cd770969376aa5b96b985328286fd12815a4721d
gaokang <gaokang@rp-pet.cn>
2022-04-12 16:34:01 +0800

    请求限流

--- src/High/RequestLimit.go
+++ src/High/RequestLimit.go
@@ -6,6 +6,8 @@ const MAXREQS = 50
 
 var sem = make(chan int, MAXREQS)
 
+// RequestLimit 可使用在接口限流、 应用限流
+//https://github.com/unknwon/the-way-to-go_ZH_CN/blob/master/eBook/14.11.md
 func RequestLimit() {
 	service := make(chan *Request)
 	go serverHandle(service)
